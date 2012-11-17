85f1a3790c0c230319585f9198be3cf34b120ee7
Alexei Boronine <alexei.boronine@gmail.com>
2012-11-17 14:54:40 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst") as readme:
 
 setup(
     name = 'apio',
-    version = "0.0.3",
+    version = "0.0.4",
     packages = ['apio'],
     description = 'Human-friendly APIs',
     license = "MIT",
