55b01b60caf211ec3c3071ba77805ab68d0d1be6
Alexei Boronine <alexei.boronine@gmail.com>
2012-11-22 20:25:19 -0500

    Implement and test apply_to_action_func

--- apio/tools.py
+++ apio/tools.py
@@ -2,7 +2,7 @@ import inspect
 from apio.exceptions import SpecError
 
 def get_arg_spec(func):
-    """ Calculate JSON schema spec for action.
+    """Calculate JSON schema spec for action.
     If function has no arguments, returns None.
     """
     # Based on the passed in function's arguments, there are
@@ -34,3 +34,48 @@ def get_arg_spec(func):
         spec["properties"][arg] = s
     return spec
         
+def apply_to_action_func(func, *obj_or_nothing):
+    """Applies a JSON object to the user-defined action function
+    based on its argument spec. The second parameter can be None
+    to mean an explicit null value or it can be omitted to let the
+    function use its default.
+    """
+    args, varargs, keywords, defaults = inspect.getargspec(func)
+    if len(args) == 0:
+        if obj_or_nothing:
+            raise SpecError("%s takes no arguments" % func.__name__)
+        return func()
+    if len(args) == 1:
+        # We weren't passed a value
+        if not obj_or_nothing:
+            # Function does not have defaults but we weren't passed a value
+            if not defaults:
+                raise SpecError("%s takes one argument" % func.__name__)
+            # We weren't passed a value, but there's a default
+            return func()
+        # We were passed a value, safe to apply regardless of defaults
+        return func(obj_or_nothing[0])
+    # func takes multiple arguments, make sure we have something to work with..
+    if not obj_or_nothing or type(obj_or_nothing[0]) is not dict:
+        raise SpecError("%s expects an object" % func.__name__)
+    obj = obj_or_nothing[0]
+    # Number of non-keyword arguments (required ones)
+    numargs = len(args)
+    if defaults:
+        numargs -= len(defaults)
+    apply_args = []
+    apply_kwargs = {}
+    for i, arg in enumerate(args):
+        # args
+        if i < numargs:
+            if arg not in obj.keys():
+                raise SpecError("%s is a required argument" % arg)
+            apply_args.append(obj.pop(arg))
+        # kwargs
+        elif arg in obj.keys():
+            apply_kwargs[arg] = obj.pop(arg)
+    # Some stuff still remaining in the object?
+    if obj:
+        raise SpecError("Unknown arguments: %s" % ", ".join(obj.keys()))
+    return func(*apply_args, **apply_kwargs)
+
--- tests/tools_test.py
+++ tests/tools_test.py
@@ -74,3 +74,61 @@ class TestGetArgSpec(TestCase):
         def f(x, y, z=1, **kwargs): pass
         with self.assertRaises(SpecError):
             get_arg_spec(f)
+
+class TestApplyToActionFunc(TestCase):
+
+    def test_no_arg_okay(self):
+        def f(): return "okay"
+        self.assertEqual(apply_to_action_func(f), "okay")
+
+    def test_no_arg_fail(self):
+        def f(): return "okay"
+        with self.assertRaises(SpecError):
+            apply_to_action_func(f, "oops")
+    
+    def test_one_arg_okay(self):
+        def f(a): return a
+        self.assertEqual(apply_to_action_func(f, 1), 1)
+
+    def test_one_arg_fail(self):
+        with self.assertRaises(SpecError):
+            def f(a): return a
+            apply_to_action_func(f)
+            
+    def test_one_kwarg_okay(self):
+        def f(a=2): return a
+        self.assertEqual(apply_to_action_func(f, 1), 1)
+        def f(a=2): return a
+        self.assertEqual(apply_to_action_func(f, {}), {})
+        def f(a=2): return a
+        self.assertEqual(apply_to_action_func(f), 2)
+
+    def test_one_kwarg_passed_none(self):
+        # None is an explicit value
+        def f(a=2): return a
+        self.assertEqual(apply_to_action_func(f, None), None)
+
+    def test_multiple_args_and_kwargs_okay(self):
+        def f(a, b=1): return a, b
+        self.assertEqual(apply_to_action_func(f, {'a': 2}), (2, 1,))
+        def f(a, b=1): return a, b
+        self.assertEqual(apply_to_action_func(f, {'a': 2, 'b': 2}), (2, 2,))
+
+    def test_multiple_kwargs_okay(self):
+        def f(a=5, b=1): return a, b
+        self.assertEqual(apply_to_action_func(f, {}), (5, 1,))
+
+    def test_unknown_kwarg(self):
+        with self.assertRaises(SpecError):
+            def f(a=5, b=1): return a, b
+            apply_to_action_func(f, {'c': 4})
+    
+    def test_not_an_object(self):
+        with self.assertRaises(SpecError):
+            def f(a=5, b=1): return a, b
+            apply_to_action_func(f, "hello")
+
+    def test_missing_required_arg(self):
+        with self.assertRaises(SpecError):
+            def f(a, b=1): return a, b
+            apply_to_action_func(f, {})
