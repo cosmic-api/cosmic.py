ef577c5ac491517b3b017ac6bc70d14614739259
Alexei Boronine <alexei.boronine@gmail.com>
2013-02-28 03:14:46 -0500

    Update copyright year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Cosmic'
-copyright = u'2012, 8313547 Canada Inc.'
+copyright = u'2013, 8313547 Canada Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
