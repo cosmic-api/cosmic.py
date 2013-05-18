36f7fa11aee8c9c83b764f6f6c881bd818c2336a
Alexei Boronine <alexei.boronine@gmail.com>
2013-05-18 18:25:30 -0400

    Add models_test.py

---
+++ tests/models_test.py
@@ -0,0 +1,18 @@
+from unittest2 import TestCase
+
+from teleport import *
+from cosmic.models import *
+
+from cosmic.api import APISerializer, ModelSerializer 
+from cosmic.actions import ActionSerializer 
+
+
+class TestCosmos(TestCase):
+
+    def setUp(self):
+        self.cosmos = Cosmos()
+
+    def test_cosmic_types(self):
+        self.assertEqual(self.cosmos["cosmic.API"], APISerializer)
+        self.assertEqual(self.cosmos["cosmic.Model"], ModelSerializer)
+        self.assertEqual(self.cosmos["cosmic.Action"], ActionSerializer)
