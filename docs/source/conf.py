46c5f3bc49cb61337490db7af20baf3ecf6541ae
Alexei Boronine <alexei.boronine@gmail.com>
2013-05-22 06:25:34 -0400

    Docs now generated from .tox environment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,6 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-source_dir = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(source_dir, '../..'))
 
 # -- General configuration -----------------------------------------------------
 
