535d1f1ea3f229a0831830c4d19e7547e2b2ddab
Alexei Boronine <alexei.boronine@gmail.com>
2013-09-07 11:36:10 -0400

    Add temporary hack to make teleport work with global Cosmos context

--- cosmic/__init__.py
+++ cosmic/__init__.py
@@ -2,9 +2,12 @@ from werkzeug.local import LocalProxy, LocalStack
 from flask import request
 
 from .models import _ctx_stack, Cosmos
+import teleport
 
 
 _global_cosmos = Cosmos()
+# Temporary hack.
+teleport._global_map = _global_cosmos
 
 def _get_current_cosmos():
     if _ctx_stack.top != None:
