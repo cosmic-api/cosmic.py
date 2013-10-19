5b900d9e8c76e13a5084b3034c69992d3aff9981
Alexei Boronine <alexei.boronine@gmail.com>
2013-10-18 21:41:16 -0400

    Move Teleport types to types module

--- cosmic/http.py
+++ cosmic/http.py
@@ -6,9 +6,6 @@ import requests
 from requests.structures import CaseInsensitiveDict
 
 from werkzeug.exceptions import HTTPException, InternalServerError, NotFound, abort
-from werkzeug.urls import url_decode, url_encode
-from werkzeug.datastructures import MultiDict
-from werkzeug.datastructures import Headers as WerkzeugHeaders
 from werkzeug.routing import Rule
 from werkzeug.routing import Map as RuleMap
 
@@ -125,72 +122,6 @@ class ServerHook(object):
         return endpoint.build_response(*args, **kwargs)
 
 
-
-
-class URLParams(ParametrizedWrapper):
-    schema = String
-
-    def __init__(self, param):
-        if type(param) == list:
-            param = OrderedDict(param)
-        self.param = param
-
-    def assemble(self, datum):
-        # Use Werkzeug to turn URL params into a dict
-        return self.from_multi_dict(url_decode(datum))
-
-    def disassemble(self, datum):
-        return url_encode(self.to_multi_dict(datum))
-
-    def from_multi_dict(self, md):
-        # Where only a single param was
-        md = md.to_dict(flat=False)
-        ret = {}
-        for name, field in self.param.items():
-            if name in md.keys():
-                if not isinstance(field['schema'], Array) and len(md[name]) == 1:
-                    ret[name] = json.loads(md[name][0])
-                else:
-                    ret[name] = []
-                    for val in md[name]:
-                        ret[name].append(json.loads(val))
-        return Struct(self.param).from_json(ret)
-
-    def to_multi_dict(self, datum):
-        d = Struct(self.param).to_json(datum)
-        md = MultiDict()
-        for name, field in self.param.items():
-            if name in d.keys():
-                md[name] = []
-                if isinstance(field['schema'], Array):
-                    md[name] = map(json.dumps, d[name])
-                else:
-                    md[name] = json.dumps(d[name])
-        return md
-
-
-class Headers(BasicWrapper):
-    schema = Array(Struct([
-        required("name", String),
-        required("value", String),
-    ]))
-
-    @classmethod
-    def assemble(cls, datum):
-        headers = WerkzeugHeaders()
-        for header in datum:
-            headers.add(header["name"], header["value"])
-        return headers
-
-    @classmethod
-    def disassemble(cls, datum):
-        headers = []
-        for name, value in datum.items():
-            headers.append({
-                "name": name,
-                "value": value
-            })
-        return headers
 
 
 def error_response(message, code):
--- cosmic/types.py
+++ cosmic/types.py
@@ -1,4 +1,78 @@
-from teleport import standard_types, required, optional, Box, ValidationError
+import json
+from werkzeug.urls import url_decode, url_encode
+from werkzeug.datastructures import MultiDict
+from werkzeug.datastructures import Headers as WerkzeugHeaders
+
+from teleport import *
+
+
+class URLParams(ParametrizedWrapper):
+    schema = String
+
+    def __init__(self, param):
+        if type(param) == list:
+            param = OrderedDict(param)
+        self.param = param
+
+    def assemble(self, datum):
+        # Use Werkzeug to turn URL params into a dict
+        return self.from_multi_dict(url_decode(datum))
+
+    def disassemble(self, datum):
+        return url_encode(self.to_multi_dict(datum))
+
+    def from_multi_dict(self, md):
+        # Where only a single param was
+        md = md.to_dict(flat=False)
+        ret = {}
+        for name, field in self.param.items():
+            if name in md.keys():
+                if not isinstance(field['schema'], Array) and len(md[name]) == 1:
+                    ret[name] = json.loads(md[name][0])
+                else:
+                    ret[name] = []
+                    for val in md[name]:
+                        ret[name].append(json.loads(val))
+        return Struct(self.param).from_json(ret)
+
+    def to_multi_dict(self, datum):
+        d = Struct(self.param).to_json(datum)
+        md = MultiDict()
+        for name, field in self.param.items():
+            if name in d.keys():
+                md[name] = []
+                if isinstance(field['schema'], Array):
+                    md[name] = map(json.dumps, d[name])
+                else:
+                    md[name] = json.dumps(d[name])
+        return md
+
+
+
+class Headers(BasicWrapper):
+    schema = Array(Struct([
+        required("name", String),
+        required("value", String),
+    ]))
+
+    @classmethod
+    def assemble(cls, datum):
+        headers = WerkzeugHeaders()
+        for header in datum:
+            headers.add(header["name"], header["value"])
+        return headers
+
+    @classmethod
+    def disassemble(cls, datum):
+        headers = []
+        for name, value in datum.items():
+            headers.append({
+                "name": name,
+                "value": value
+            })
+        return headers
+
+
 
 def getter(name):
     from .api import API
