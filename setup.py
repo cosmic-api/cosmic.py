a887a74edf28eb8e4076b45de0ef5bf351061a37
Alexei Boronine <alexei@boronine.com>
2014-07-24 07:54:32 +0200

    Bump alpha version

--- setup.py
+++ setup.py
@@ -7,18 +7,21 @@ with open("README.rst") as readme:
 
 setup(
     name = 'cosmic',
-    version = "0.1.4",
+    version = "0.2.0.alpha1",
     url = "http://www.cosmic-api.com/docs/cosmic/python/",
     packages = ['cosmic'],
     description = 'A high-level web API framework',
     license = "MIT",
     author = "8313547 Canada Inc.",
-    author_email = "alexei.boronine@gmail.com",
+    author_email = "alexei@boronine.com",
     long_description = long_description,
     install_requires = [
         "teleport==0.2.1",
         "Flask==0.10",
         "Werkzeug==0.9.1",
         "requests==2.0.0",
-    ]
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+    ],
 )
